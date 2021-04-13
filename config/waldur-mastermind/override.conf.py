21bbd0e18c5afe32cd5526c22b4b79b494a8d4ae
Victor Mireyev <victor@opennodecloud.com>
2021-04-13 14:48:07 +0300

    Drop leftover.

--- config/waldur-mastermind/override.conf.py
+++ config/waldur-mastermind/override.conf.py
@@ -1,13 +1,5 @@
-# This file is managed by Ansible, manual changes will be overwritten.
-#
 # Configuration for Waldur plugins
-#
-# Django
-#
-
-#
-# waldur-core
-# https://opennode.atlassian.net/wiki/display/WD/MasterMind+configuration#MasterMindconfiguration-Additionalsettings
+# https://waldur.github.io/waldur-docs/admin-guide/mastermind-configuration/configuration-guide/
 
 
 WALDUR_CORE['AUTHENTICATION_METHODS'] = ["LOCAL_SIGNIN"]
@@ -17,10 +9,6 @@ WALDUR_CORE['SITE_NAME'] = 'test'
 WALDUR_CORE['SITE_LOGO'] = '/etc/waldur/logo.png'
 
 WALDUR_CORE['CREATE_DEFAULT_PROJECT_ON_ORGANIZATION_CREATION'] = True
-
-
-# Disable company types till we start making use of them
-WALDUR_CORE['COMPANY_TYPES'] = ()
 
 # Disable geoip location till HomePort releases maps to a stable deployment
 WALDUR_CORE['ENABLE_GEOIP'] = False
