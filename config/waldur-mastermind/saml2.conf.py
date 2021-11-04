31698e7fe39540b224d873426c26ac39ced461ba
Ilja Livenson <ilja@opennodecloud.com>
2021-11-04 07:29:47 +0000

    Fix variables

--- config/waldur-mastermind/saml2.conf.py
+++ config/waldur-mastermind/saml2.conf.py
@@ -1,7 +1,7 @@
 # Custom SAML2 configuration
 WALDUR_AUTH_SAML2.update({
     # PEM formatted certificate chain file
-    'cert_file': '/etc/waldur/saml2/credentials/sp.crt',
+    'CERT_FILE': '/etc/waldur/saml2/credentials/sp.crt',
     # PEM formatted certificate key file
-    'key_file': '/etc/waldur/saml2/credentials/sp.pem',
+    'KEY_FILE': '/etc/waldur/saml2/credentials/sp.pem',
 })
