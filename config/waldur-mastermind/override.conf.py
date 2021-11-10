19619b58136394c41147acf95e38a1321470ed0a
Victor Mireyev <victor.mireyev@gmail.com>
2021-11-10 18:59:27 +0200

    Drop support for checking invitation token in landing view.

--- config/waldur-mastermind/override.conf.py
+++ config/waldur-mastermind/override.conf.py
@@ -6,8 +6,5 @@ WALDUR_CORE['SITE_LOGO'] = '/etc/waldur/logo.png'
 
 WALDUR_CORE['CREATE_DEFAULT_PROJECT_ON_ORGANIZATION_CREATION'] = True
 
-
 # Disable geoip location till HomePort releases maps to a stable deployment
 WALDUR_CORE['ENABLE_GEOIP'] = False
-
-WALDUR_CORE['ALLOW_SIGNUP_WITHOUT_INVITATION'] = False
