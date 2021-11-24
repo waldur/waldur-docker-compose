4444abe3f91583575727b0a5b1610050f490f278
Yefim Gasel <yefim@opennodecloud.com>
2021-11-24 12:00:57 +0000

    Add keycloak integration

--- config/waldur-mastermind/override.conf.py
+++ config/waldur-mastermind/override.conf.py
@@ -1,4 +1,4 @@
-WALDUR_CORE['AUTHENTICATION_METHODS'] = ["LOCAL_SIGNIN"]
+WALDUR_CORE['AUTHENTICATION_METHODS'] = ["LOCAL_SIGNIN", "SOCIAL_SIGNUP"]
 
 WALDUR_CORE['SITE_NAME'] = 'test'
 
@@ -8,3 +8,10 @@ WALDUR_CORE['CREATE_DEFAULT_PROJECT_ON_ORGANIZATION_CREATION'] = True
 
 # Disable geoip location till HomePort releases maps to a stable deployment
 WALDUR_CORE['ENABLE_GEOIP'] = False
+
+WALDUR_AUTH_SOCIAL.update({'KEYCLOAK_AUTH_URL': '/auth/realms/Waldur/protocol/openid-connect/auth',
+ 'KEYCLOAK_CLIENT_ID': 'waldur',
+ 'KEYCLOAK_SECRET': env.get('KEYCLOAK_SECRET'),
+ 'KEYCLOAK_TOKEN_URL': 'http://keycloak:8080/auth/realms/Waldur/protocol/openid-connect/token',
+ 'KEYCLOAK_USERINFO_URL': 'http://keycloak:8080/auth/realms/Waldur/protocol/openid-connect/userinfo'
+})
