1f159aeb4b29abc7e9715f615729ce35a973e64a
Ilja Livenson <ilja.livenson@gmail.com>
2025-12-22 20:21:24 +0200

    Do not override logging settings by default

--- config/waldur-mastermind/logging.conf.py
+++ config/waldur-mastermind/logging.conf.py
@@ -1,41 +1,28 @@
-import sys
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-
-    'formatters': {
-        'simple': {
-            'format': '%(asctime)s %(levelname)s %(message)s',
-        },
-    },
-
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-            'level': 'DEBUG',
-            'stream': sys.stdout,
-        },
-    },
-
-    'loggers': {
-        # Suppress excessive Celery task registration logging
-        'celery.utils.imports': {
-            'level': 'WARNING',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'celery.app.autodiscover': {
-            'level': 'WARNING',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-    },
-
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['console'],
-    },
-}
+# Empty logging configuration - uses defaults from Docker image
+# Located at: docker/rootfs/etc/waldur/logging.conf.py in the main repository
+#
+# To customize logging, uncomment and modify the LOGGING dictionary below:
+#
+# import sys
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'simple': {
+#             'format': '%(asctime)s %(levelname)s %(message)s',
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple',
+#             'level': 'DEBUG',
+#             'stream': sys.stdout,
+#         },
+#     },
+#     'root': {
+#         'level': 'DEBUG',
+#         'handlers': ['console'],
+#     },
+# }
